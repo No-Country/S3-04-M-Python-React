adf06e35e947028d7e12f751b88e2fa6d772f425
Gabriez <gabrieltorres2014@gmail.com>
2022-08-22 00:58:03 -0400

    finished questions

--- urclothes/reactapp/src/page/preguntas.js
+++ urclothes/reactapp/src/page/preguntas.js
@@ -1,148 +1,144 @@
 import React from 'react'
 import {Form, Button} from 'react-bootstrap';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 export default function Questions () {
   return (
     <div className='container-fluid m-auto'>
-    <div className='lrContainer p-3 w-50'>
-        <h3>Responde el siguiente cuestionario</h3>
+    <div className='lrContainer p-3 w-50 mb-3'>
+        <h3 className='text-center'>Responde el siguiente cuestionario</h3>
     <Form>
-        <Form.Group className='mb-4'>
-            <Form.Text>Primera pregunta</Form.Text>
-            <Form.Check
-            type='radio'
-            name='first'
-            id='first-1'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='first-1'>1</Form.Check.Label>
-            
-            <Form.Check
-            type='radio'
-            name='first'
-            id='first-2'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='first-2'>2</Form.Check.Label>
-            
-            <Form.Check
-            type='radio'
-            name='first'
-            id='first-3'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='first-3'>3</Form.Check.Label>
-            
-            <Form.Check
-            type='radio'
-            name='first'
-            id='first-4'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='first-4'>4</Form.Check.Label>
+        <Form.Group className='mb-2'>
+            <p className='mb-3'>Primera pregunta</p>
+           <ButtonGroup className="d-flex flex-column w-50">
+            <ToggleButton
+            className='mb-3 rounded' type='radio' 
+            variant='outline-success'
+            name='first'>1</ToggleButton>
+            <ToggleButton
+            className='mb-3 rounded' type='radio' 
+            variant='outline-success'
+            name='first'>2</ToggleButton>
+            <ToggleButton
+            className='mb-3 rounded' type='radio' 
+            variant='outline-success'
+            name='first'>3</ToggleButton>
+            <ToggleButton
+            className='mb-3 rounded' type='radio' 
+            variant='outline-success'
+            name='first'>4</ToggleButton>
+           </ButtonGroup>
+
+
         </Form.Group>
-        <Form.Group className='mb-4'>
-            <Form.Text>Segunda pregunta</Form.Text>
-            <Form.Check
+        <Form.Group className='mb-2'>
+            <p className='mb-3'>Segunda pregunta</p>
+            <ButtonGroup className="d-flex flex-column w-50">
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='second'
             id='second-1'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='second-1'>1</Form.Check.Label>
+            variant='outline-success' 
+            >1</ToggleButton>
             
-            <Form.Check
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='second'
             id='second-2'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='second-2'>2</Form.Check.Label>
+            variant='outline-success' 
+            >2</ToggleButton>
             
-            <Form.Check
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='second'
             id='second-3'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='second-3'>3</Form.Check.Label>
+            variant='outline-success' 
+            >3</ToggleButton>
             
-            <Form.Check
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='second'
             id='second-4'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='second-4'>4</Form.Check.Label>
+            variant='outline-success' 
+            >4</ToggleButton>
+            </ButtonGroup>
         </Form.Group>
-        <Form.Group className='mb-4'>
-            <Form.Text>Tercera pregunta</Form.Text>
-            <Form.Check
+        <Form.Group className='mb-2'>
+            <p className='mb-3'>Tercera pregunta</p>
+            <ButtonGroup className="d-flex flex-column w-50">
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='third'
             id='third-1'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='third-1'>1</Form.Check.Label>
-            
-            <Form.Check
+            variant='outline-success' 
+            >1</ToggleButton>            
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='third'
             id='third-2'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='third-2'>2</Form.Check.Label>
-            
-            <Form.Check
+            variant='outline-success' 
+            >2</ToggleButton>            
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='third'
             id='third-3'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='third-3'>3</Form.Check.Label>
-            
-            <Form.Check
+            variant='outline-success' 
+            >3</ToggleButton>            
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='third'
             id='third-4'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='third-4'>4</Form.Check.Label>
-        </Form.Group>
-        <Form.Group className='mb-4'>
-            <Form.Text>Cuarta pregunta</Form.Text>
-            <Form.Check
+            variant='outline-success' 
+            >4</ToggleButton>
+            </ButtonGroup>       
+             </Form.Group>
+        <Form.Group className='mb-2'>
+            <p className='mb-3'>Cuarta pregunta</p>
+            <ButtonGroup className="d-flex flex-column w-50">
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='fourth'
             id='fourth-1'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='fourth-1'>1</Form.Check.Label>
+            variant='outline-success' 
+            >1</ToggleButton>
             
-            <Form.Check
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='fourth'
             id='fourth-2'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='fourth-2'>2</Form.Check.Label>
+            variant='outline-success' 
+            >2</ToggleButton>
             
-            <Form.Check
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='fourth'
             id='fourth-3'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='fourth-3'>3</Form.Check.Label>
+            variant='outline-success' 
+            >3</ToggleButton>
             
-            <Form.Check
+            <ToggleButton
+            className='mb-3 rounded'
             type='radio'
             name='fourth'
             id='fourth-4'
-            class="btn-check" 
-            />
-            <Form.Check.Label className='btn btn-outline-success' htmlFor='fourth-4'>4</Form.Check.Label>
+            variant='outline-success' 
+            >4</ToggleButton>
+            </ButtonGroup>
         </Form.Group>
+
+        <Button as="input" type="submit" value="Enviar" />
     </Form>
     </div>
     </div>
